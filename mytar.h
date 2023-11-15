695e0a25da555d5c0572ae5327ebbff24345e76f
sofiamorris <97552410+sofiamorris@users.noreply.github.com>
2023-11-15 11:21:32 -0800

    Create mytar.h

---
+++ mytar.h
@@ -0,0 +1,22 @@
+typedef struct __attribute__ ((__packed__)) header_t{
+	char name[101];
+	char mode[8];
+	char uid[8];
+	char gid[8];
+	char size[12];
+	char mtime[12];
+	char chksum[8];
+	char typeflag[1];
+	char linkname[100];
+	char magic[6];
+	char version[2];
+	char uname[32];
+	char gname[32];
+	char devmajor[8];
+	char devminor[8];
+	char prefix[155];
+}header;
+
+typedef struct archive_t{
+
+}archive;
