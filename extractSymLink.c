27848dca1e6359993f7871fc362d5866ee3c2d4e
Sofia Emelia Morris <smorri33@unix3.csc.calpoly.edu>
2023-11-27 10:22:47 -0800

    extractSymLink.c

--- extractSymLink.c
+++ extractSymLink.c
@@ -8,18 +8,12 @@
 #include <sys/time.h>
 
 void extractSymLink(char *name, char *linkname, time_t mtime, int v){
-    struct utimbuf times;
     char copy[PATH_MAX];
 
     strcpy(copy,name); 
     makeDirHier(copy);
     if (symlink(linkname, name) == -1){
         perror("Error creating symlink");
-        exit(EXIT_FAILURE);
-    }
-    times.modtime = mtime;
-    if (utime(name, &times) == -1) {
-        perror("error setting mtime");
         exit(EXIT_FAILURE);
     }
     if (v){
